7cda490af9e1b910bdeaaf92cdf2f720a2892fa9
Do Van Dat (K16_HL) <datdvhe161664@fpt.edu.vn>
2025-01-21 22:29:26 +0700

    Create project and make Home, Shop,Cart, Checkout, Product Detail ,Authentication Page View

---
+++ Crystal_Eyes_Controller/Controllers/CustomerController.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Crystal_Eyes_Controller.Controllers
+{
+	public class CustomerController : Controller
+	{
+		public IActionResult Login()
+		{
+			return View();
+		}
+		public IActionResult Register()
+		{
+			return View();
+		}
+		public IActionResult OTP()
+		{
+			return View();
+		}
+		public IActionResult ForgotPassword()
+		{
+			return View();
+		}
+		public IActionResult ResetPassword()
+		{
+			return View();
+		}
+		public IActionResult ChangePassword()
+		{
+			return View();
+		}
+
+		public IActionResult Shop()
+		{
+			return View();
+		}
+
+		public IActionResult OrderDetail()
+		{
+			return View();
+		}
+
+		public IActionResult HistoryOrder()
+		{
+			return View();
+		}
+
+		public IActionResult Checkout()
+		{
+			return View();
+		}
+
+		public IActionResult EditProfile()
+		{
+			return View();
+		}
+
+		public IActionResult Profile()
+		{
+			return View();
+		}
+		public IActionResult Wishlist()
+		{
+			return View();
+		}
+        public IActionResult ProductDetail()
+        {
+            return View();
+        }
+    }
+}
---
+++ Crystal_Eyes_Controller/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Crystal_Eyes_Controller.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
