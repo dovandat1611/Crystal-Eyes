3041551a23bc94aa21553329d8da2f76a97ed0fe
Do Van Dat <dovandat1611@gmail.com>
2025-02-05 21:51:04 +0700

    done and publish project

--- Crystal_Eyes_Controller/Program.cs
+++ Crystal_Eyes_Controller/Program.cs
@@ -50,12 +50,12 @@ builder.Services.Configure<MailSettings>(mailSettingsSection); // register for d
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
+//if (!app.Environment.IsDevelopment())
+//{
+//    app.UseExceptionHandler("/Home/Error");
+//    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+//    app.UseHsts();
+//}
 
 // Phải đặt trước các middleware khác
 app.UseSession();
--- Crystal_Eyes_Controller/Services/WishlistService.cs
+++ Crystal_Eyes_Controller/Services/WishlistService.cs
@@ -7,7 +7,6 @@ namespace Crystal_Eyes_Controller.Services
 	{
 		public async Task<(string message, string action, int totalWishlist)> ProcessWishlistAsync(int productId, HttpContext context)
 		{
-			// Lấy cookie "wishlist" (nếu có)
 			var wishlistCookie = context.Request.Cookies["wishlist"];
 			List<int> wishlist = wishlistCookie != null
 				? JsonConvert.DeserializeObject<List<int>>(wishlistCookie)
@@ -18,29 +17,21 @@ namespace Crystal_Eyes_Controller.Services
 
 			if (wishlist.Contains(productId))
 			{
-				// Nếu sản phẩm đã có trong wishlist, xóa nó
 				wishlist.Remove(productId);
 				message = "Xóa thành công sản phẩm ra khỏi Yêu Thích";
 				action = "Delete";
 			}
 			else
 			{
-				// Nếu sản phẩm chưa có trong wishlist, thêm vào danh sách
 				wishlist.Add(productId);
 				message = "Thêm thành công sản phẩm vào Yêu Thích";
 				action = "Create";
 			}
 
-			// Lưu danh sách wishlist vào cookie (set thời gian hết hạn là 30 ngày)
-			var cookieOptions = new CookieOptions
-			{
-				Expires = DateTime.Now.AddDays(30),
-				HttpOnly = true,
-				Secure = true  // Chỉ dùng trong môi trường HTTPS
-			};
-			context.Response.Cookies.Append("wishlist", JsonConvert.SerializeObject(wishlist), cookieOptions);
+			context.Response.Cookies.Append("wishlist", JsonConvert.SerializeObject(wishlist));
 
 			return (message, action, wishlist.Count);
 		}
+
 	}
 }
